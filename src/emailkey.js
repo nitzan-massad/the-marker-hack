0226e69f3abbff1a4603264a8704c9e75475b3d0
Nitzan Massad <nitzanmassad@gmail.com>
2021-09-11 18:48:11 +0300

    fix build problems

--- src/emailkey.js
+++ src/emailkey.js
@@ -1,5 +1,7 @@
-export default {
+const emailKeys = {
     USER_ID: `user_bQuPR7rEj8ZATM08WyYW6`, //userID
     TEMPLATE_ID: `template_3c7voko`,         //templateID
     SERVICE_ID: 'service_vtgnux3'
 }
+
+export default emailKeys
